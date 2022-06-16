0182d7e7f6ddf7cd658daa9115621fa06f65ba17
aniyahmb99 <aniyah_mb@yahoo.com>
2022-06-16 17:48:36 -0400

    fixed server errors and mongoose connection

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // routes
